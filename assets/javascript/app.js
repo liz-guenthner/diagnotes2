2a64aadf83c2bb4ee06fc178da454898c1863ea8
Liz Guenthner <liz.guenthner@t-3.com>
2019-09-26 11:39:54 -0500

    create repo

---
+++ assets/javascript/app.js
@@ -0,0 +1,290 @@
+$(document).ready(function() {
+  //CREATE DIV TO RENDER THE SELECTED APPOINTMENT'S PATIENT NAME AND DATE
+ $("#appointmentInfo").empty(); 
+ var appointmentInfoDiv = $("<div>");  
+ appointmentInfoDiv.attr({
+   "class": "alert alert-info",
+   "role": "alert"
+ });
+ //RETRIEVE PATIENT NAME AND APPOINTMENT DATE FROM LOCAL STORAGE AND APPEND TO DIV
+ if(localStorage.getItem("currentPatientName")){
+ var appointmentPatientName = localStorage.getItem("currentPatientName");
+ var appointmentDate = localStorage.getItem("currentApptDate"); 
+ appointmentInfoDiv.html("<p>Patient Name: " + appointmentPatientName + " · Appointment Date: " + appointmentDate + "</p>");
+ $("#appointmentInfo").append(appointmentInfoDiv);
+ } else {
+  appointmentInfoDiv.html("<p>Please go back to calendar and select an appointment before proceeding.</p>");
+  $("#appointmentInfo").append(appointmentInfoDiv);
+ }
+
+
+
+ var queryurl="https://sv443.net/jokeapi/category/Miscellaneous?blacklistFlags=religious,nsfw,political"
+
+ $.ajax({
+     url: queryurl,
+    method: "GET"
+}).then(function (response) {
+    $("#setup").text(response.setup);
+    $("#delivery").text(response.delivery);
+    $("#joke").text(response.joke);
+  });
+
+ try {
+  var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  var recognition = new SpeechRecognition();
+}
+catch(e) {
+  console.error(e);
+  $('.no-browser-support').show();
+  $('.app').hide();
+}
+
+var noteFireKey = localStorage.getItem("currentFireKey");
+var keyLength= noteFireKey.length;
+
+var noteTextarea = $('#note-textarea');
+var instructions = $('#recording-instructions');
+var notesList = $('ul#notes');
+
+var noteContent = '';
+
+// GET ALL NOTE SNIPPETS FOR THIS APPOINTMENT AND DISPLAY THEM
+var notes = getAllNotes();
+// console.log(notes);
+renderNotes(notes);
+
+
+/*-----------------------------
+      Voice Recognition 
+------------------------------*/
+
+// If false, the recording will stop after a few seconds of silence.
+// When true, the silence period is longer (about 15 seconds),
+// allowing us to keep recording even when the user pauses. 
+recognition.continuous = true;
+
+// This block is called every time the Speech APi captures a line. 
+recognition.onresult = function(event) {
+
+  // event is a SpeechRecognitionEvent object.
+  // It holds all the lines we have captured so far. 
+  // We only need the current one.
+  var current = event.resultIndex;
+
+  // Get a transcript of what was said.
+  var transcript = event.results[current][0].transcript;
+
+  // Add the current transcript to the contents of our Note.
+  // There is a weird bug on mobile, where everything is repeated twice.
+  // There is no official solution so far so we have to handle an edge case.
+  var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
+
+  if(!mobileRepeatBug) {
+    noteContent += transcript;
+    noteTextarea.val(noteContent);
+  }
+};
+
+recognition.onstart = function() { 
+  instructions.text('Voice recognition activated. Try speaking into the microphone.');
+}
+
+recognition.onspeechend = function() {
+  instructions.text('You were quiet for a while so voice recognition turned itself off.');
+}
+
+recognition.onerror = function(event) {
+  if(event.error == 'no-speech') {
+    instructions.text('No speech was detected. Try again.');  
+  };
+}
+
+
+
+/*-----------------------------
+      App buttons and input 
+------------------------------*/
+
+$('#start-record-btn').on('click', function(e) {
+  if (noteContent.length) {
+    noteContent += ' ';
+  }
+  recognition.start();
+});
+
+
+$('#pause-record-btn').on('click', function(e) {
+  recognition.stop();
+  instructions.text('Voice recognition paused.');
+});
+
+// Sync the text inside the text area with the noteContent variable.
+noteTextarea.on('input', function() {
+  noteContent = $(this).val();
+})
+
+$('#save-note-btn').on('click', function(e) {
+  recognition.stop();
+
+  if(!noteContent.length) {
+    instructions.text('Could not save empty note. Please add a message to your note.');
+  }
+  else {
+    // Save note to localStorage.
+    // The key is the dateTime with seconds, the value is the content of the note.
+    saveNote(new Date().toLocaleString(), noteContent);
+
+    // Reset variables and update UI.
+    noteContent = '';
+    renderNotes(getAllNotes());
+    noteTextarea.val('');
+    instructions.text('Note saved successfully.');
+
+
+  }
+  
+    
+      
+})
+
+
+notesList.on('click', function(e) {
+  e.preventDefault();
+  var target = $(e.target);
+
+  // Listen to the selected note.
+  if(target.hasClass('listen-note')) {
+    var content = target.closest('.note').find('.content').text();
+    readOutLoud(content);
+  }
+
+  // Delete note.
+  if(target.hasClass('delete-note')) {
+    var dateTime = target.siblings('.date').text();  
+    deleteNote(dateTime);
+    target.closest('.note').remove();
+  }
+});
+
+
+
+/*-----------------------------
+      Speech Synthesis 
+------------------------------*/
+
+function readOutLoud(message) {
+	var speech = new SpeechSynthesisUtterance();
+
+  // Set the text and voice attributes.
+	speech.text = message;
+	speech.volume = 1;
+	speech.rate = 1;
+	speech.pitch = 1;
+  
+	window.speechSynthesis.speak(speech);
+}
+
+
+
+/*-----------------------------
+      Helper Functions 
+------------------------------*/
+
+function renderNotes(notes) {
+  // console.log(notes);
+  var html = '';
+  if(notes.length) {
+    notes.forEach(function(note) {
+      html+= `<li class="note">
+        <p class="header">
+          <span class="date">${note.date}</span>
+          <a href="#" class="listen-note" title="Listen to Note">Listen to Note</a>
+          <a href="#" class="delete-note" title="Delete">Delete</a>
+        </p>
+        <p class="content">${note.content}</p>
+      </li>`;    
+    });
+  }
+  else {
+    html = '<li><p class="content">You don\'t have any notes yet.</p></li>';
+  }
+  notesList.html(html);
+}
+
+//ADD FIREBASE KEY TO NOTES TO SERVE AS UNIQUE IDENTIFIER PER APPOINTMENT
+function saveNote(dateTime, content) {
+  localStorage.setItem(noteFireKey +' note-' + dateTime, content);
+}
+
+//DYNAMICALLY ADJUST THE CONDITIONAL TO 
+//ENSURE APPENDED NOTES MATCH THIS APPOINTMENTS UNIQUE KEY
+function getAllNotes() {
+  var notes = [];
+  var key;
+  for (var i = 0; i < localStorage.length; i++) {
+    key = localStorage.key(i);
+
+    if(key.substring(0,keyLength + 6) == noteFireKey +' note-') {
+      notes.push({
+        date: key.replace(noteFireKey + ' note-',''),
+        content: localStorage.getItem(localStorage.key(i))
+      });
+    } 
+  }
+  return notes;
+}
+
+
+function deleteNote(dateTime) {
+  localStorage.removeItem('note-' + dateTime); 
+}
+
+//Event listner for submit button for our medication information form//
+$("#submit").click( function(event) {
+  event.preventDefault();
+  $("#addmedication").empty()
+  var newMedication = $("#medication-input").val()
+  var addmedication = $("#medication-input").val()
+  $("#addmedication").text(addmedication)
+  $("#newMedication").text(newMedication)
+  clear()
+  //declaring variable for api search. This searches for the name of medication.//
+  var medicationName = addmedication
+  var queryURL = "https://api.fda.gov/drug/label.json?search=" + medicationName + "&api_key=YfbsJ7YnoFpyh1HsPKea9VmisCfwmgFV2lpWKDJC"
+  console.log(queryURL)
+//ajax call using our queryURL and medication name//
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+//This is the response from the call and the info is then displayed on our page//
+  .then(function(response) {
+    console.log(response.results[0])
+    $("#medicationReaction").text(response.results[0].adverse_reactions);
+    $("#medicationPurpose").text(response.results[0].purpose);
+    $("#medicationIngredients").text(response.results[0].active_ingredient);
+    $("#medicationDosage").text(response.results[0].dosage_and_administration);
+      
+  });
+
+//DISPLAY NOTES ON THE PAGE
+$("#notesContainer").append(notes);
+
+});
+//This function clears the input value and empties the divs each time new medication is added//
+function clear() {
+  $('input[type="text"]').val('');
+  $('#medication-input').val('');
+  $('#medicationReaction').empty();
+  $('#medicationPurpose').empty();
+  $('#medicationIngredients').empty();
+  $('#medicationDosage').empty();
+  };
+
+
+});
+//END OF DOCUMENT READY FUNCTION
+
+
+
---
+++ assets/javascript/caltemplatescript.js
@@ -0,0 +1,319 @@
+//SET ALL GLOBAL VARIABLES
+var calendar = document.getElementById("calendar-table");
+var gridTable = document.getElementById("table-body");
+var currentDate = new Date();
+var selectedDate = currentDate;
+var selectedDayBlock = null;
+var globalEventObj = {};
+
+//VISUALLY SHOW WHICH DATE HAS BEEN SELECTED BY USING JQUERYUI TO FADE IN/OUT
+$(document).on("click", ".row > .col", function(){
+   $(".row > .col").animate({"backgroundColor":"#FFFFFF"});
+   $(this).animate({"backgroundColor":"#89e6e1"});
+});
+
+//IMPORT CALENDAR TEMPLATE FROM CODEPEN
+function createCalendar(date, side) {
+   var currentDate = date;
+   var startDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+
+   var monthTitle = document.getElementById("month-name");
+   var monthName = currentDate.toLocaleString("en-US", {
+      month: "long"
+   });
+   var yearNum = currentDate.toLocaleString("en-US", {
+      year: "numeric"
+   });
+   monthTitle.innerHTML = `${monthName} ${yearNum}`;
+
+   if (side == "left") {
+      gridTable.className = "animated fadeOutRight";
+   } else {
+      gridTable.className = "animated fadeOutLeft";
+   }
+
+   setTimeout(() => {
+      gridTable.innerHTML = "";
+
+      var newTr = document.createElement("div");
+      newTr.className = "row";
+      var currentTr = gridTable.appendChild(newTr);
+
+      for (var i = 1; i < startDate.getDay(); i++) {
+         var emptyDivCol = document.createElement("div");
+         emptyDivCol.className = "col empty-day";
+         currentTr.appendChild(emptyDivCol);
+      }
+
+      var lastDay = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+      lastDay = lastDay.getDate();
+
+      for (var i = 1; i <= lastDay; i++) {
+         if (currentTr.children.length >= 7) {
+            currentTr = gridTable.appendChild(addNewRow());
+         }
+         var currentDay = document.createElement("div");
+         currentDay.className = "col";
+         if (selectedDayBlock == null && i == currentDate.getDate() || selectedDate.toDateString() == new Date(currentDate.getFullYear(), currentDate.getMonth(), i).toDateString()) {
+            selectedDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), i);
+
+            selectedDayBlock = currentDay;
+            setTimeout(() => {
+               currentDay.classList.add("blue");
+               currentDay.classList.add("lighten-3");
+            }, 900);
+         }
+         currentDay.innerHTML = i;
+
+         //show marks & appt links
+         if (globalEventObj[new Date(currentDate.getFullYear(), currentDate.getMonth(), i).toDateString()]) {
+            var eventMark = document.createElement("div");
+            eventMark.className = "day-mark";
+            currentDay.appendChild(eventMark);
+            var newApptLink = document.createElement("div");
+            newApptLink.className = "appt-link";
+            currentDay.appendChild(newApptLink);
+
+         }
+
+         currentTr.appendChild(currentDay);
+      }
+
+      for (var i = currentTr.getElementsByTagName("div").length; i < 7; i++) {
+         var emptyDivCol = document.createElement("div");
+         emptyDivCol.className = "col empty-day";
+         currentTr.appendChild(emptyDivCol);
+      }
+
+      if (side == "left") {
+         gridTable.className = "animated fadeInLeft";
+      } else {
+         gridTable.className = "animated fadeInRight";
+      }
+
+      function addNewRow() {
+         var node = document.createElement("div");
+         node.className = "row";
+         return node;
+      }
+
+   }, !side ? 0 : 270);
+}
+
+createCalendar(currentDate);
+
+var todayDayName = document.getElementById("todayDayName");
+todayDayName.innerHTML = "Today is " + currentDate.toLocaleString("en-US", {
+   weekday: "long",
+   day: "numeric",
+   month: "short"
+});
+
+var prevButton = document.getElementById("prev");
+var nextButton = document.getElementById("next");
+
+prevButton.onclick = function changeMonthPrev() {
+   currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1);
+   createCalendar(currentDate, "left");
+}
+nextButton.onclick = function changeMonthNext() {
+   currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1);
+   createCalendar(currentDate, "right");
+}
+
+function addEvent(title, desc) {
+   if (!globalEventObj[selectedDate.toDateString()]) {
+      globalEventObj[selectedDate.toDateString()] = {};
+   }
+   globalEventObj[selectedDate.toDateString()][title] = desc;
+}
+
+function showEvents() {
+   var objWithDate = globalEventObj[selectedDate.toDateString()];
+
+   if (objWithDate) {
+      var eventsCount = 0;
+      for (key in globalEventObj[selectedDate.toDateString()]) {
+         var eventContainer = document.createElement("div");
+         eventContainer.className = "eventCard";
+
+         var eventHeader = document.createElement("div");
+         eventHeader.className = "eventCard-header";
+
+         var eventDescription = document.createElement("div");
+         eventDescription.className = "eventCard-description";
+
+         eventHeader.append(document.createTextNode(key));
+         eventContainer.appendChild(eventHeader);
+
+         eventDescription.appendChild(document.createTextNode(objWithDate[key]));
+         eventContainer.appendChild(eventDescription);
+
+         var markWrapper = document.createElement("div");
+         markWrapper.className = "eventCard-mark-wrapper";
+         var mark = document.createElement("div");
+         mark.classList = "eventCard-mark";
+         markWrapper.appendChild(mark);
+         eventContainer.appendChild(markWrapper);
+         eventsCount++;
+      }
+      var emptyFormMessage = document.getElementById("emptyFormTitle");
+      emptyFormMessage.innerHTML = `${eventsCount} events now`;
+   } else {
+      var emptyMessage = document.createElement("div");
+      emptyMessage.className = "empty-message";
+      emptyMessage.innerHTML = "Sorry, no events to selected date";
+      var emptyFormMessage = document.getElementById("emptyFormTitle");
+      emptyFormMessage.innerHTML = "No events now";
+   }
+}
+
+gridTable.onclick = function (e) {
+
+   if (!e.target.classList.contains("col") || e.target.classList.contains("empty-day")) {
+      return;
+   }
+
+   if (selectedDayBlock) {
+      if (selectedDayBlock.classList.contains("blue") && selectedDayBlock.classList.contains("lighten-3")) {
+         selectedDayBlock.classList.remove("blue");
+         selectedDayBlock.classList.remove("lighten-3");
+      }
+   }
+   selectedDayBlock = e.target;
+   selectedDayBlock.classList.add("blue");
+   selectedDayBlock.classList.add("lighten-3");
+   selectedDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), parseInt(e.target.innerHTML));
+
+   showEvents();
+}
+
+var changeFormButton = document.getElementById("changeFormButton");
+var addForm = document.getElementById("addForm");
+changeFormButton.onclick = function (e) {
+   addForm.style.top = 0;
+}
+
+var cancelAdd = document.getElementById("cancelAdd");
+cancelAdd.onclick = function (e) {
+   addForm.style.top = "100%";
+   var inputs = addForm.getElementsByTagName("input");
+   for (var i = 0; i < inputs.length; i++) {
+      inputs[i].value = "";
+   }
+   var labels = addForm.getElementsByTagName("label");
+   for (var i = 0; i < labels.length; i++) {
+      labels[i].className = "";
+   }
+}
+
+//FIREBASE CONFIG
+var firebaseConfig = {
+   apiKey: "AIzaSyDzhSWIeHaVKr2ipKn4kQYXUI-_d_TnE6I",
+   authDomain: "diagnotes-project-1.firebaseapp.com",
+   databaseURL: "https://diagnotes-project-1.firebaseio.com",
+   projectId: "diagnotes-project-1",
+   storageBucket: "",
+   messagingSenderId: "227854326970",
+   appId: "1:227854326970:web:aab38cfa0ff6ff62"
+ };
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+ var database = firebase.database();
+
+
+//STORE PATIENT NAME, APPOINTMENT DESCRIPTION, AND APPOINTMENT DATE IN FIREBASE
+//EVERY TIME AN APPOINTMENT IS SCHEDULED
+var globalObjectCopy;
+$("#addEventButton").on("click", function(event){
+   event.preventDefault();
+   var patientName=$("#patientNameInput").val().trim();
+   var apptDescrip=$("#eventDescInput").val().trim();
+   globalObjectCopy = globalEventObj;
+   var apptDate = selectedDate.toLocaleString("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric"
+   });
+
+
+//SETTING UP OBJECT TO PUSH TO FIREBASE
+var apptDetails = {
+      patientName: patientName,
+      apptDescrip: apptDescrip,
+      apptDate: apptDate
+   };
+
+   database.ref().push(apptDetails);
+   addForm.style.top = "100%";
+});
+//END OF FIREBASE STORAGE ON CLICK EVENT
+
+
+//RENDER APPOINTMENTS STORED IN FIREBASE ON THE PAGE W/ UNIQUE IDENTIFIERS
+var firepName;
+var fireDescrip;
+var fireDate;
+var newApptLink;
+database.ref().on("child_added", function(childSnapshot){
+   firepName = childSnapshot.val().patientName;
+   fireDescrip = childSnapshot.val().apptDescrip;
+   fireDate = childSnapshot.val().apptDate;
+   newApptLink = $("<a>");
+   newApptLink.attr({"href": "appoinment.html",
+      "data-fireChildKey": childSnapshot.key,
+      "data-patientName": childSnapshot.val().patientName,
+      "data-apptDate": childSnapshot.val().apptDate,
+      "class": "list-group-item list-group-item-action"
+      });
+   newApptLink.append("<p>" + firepName + ": " + fireDate + " - " + fireDescrip + "</p>");
+   $("#apptList").prepend(newApptLink);
+});
+
+var addEventButton = document.getElementById("addEventButton");
+addEventButton.onclick = function (e) {
+   
+   var title = firepName;
+   var desc = fireDescrip;
+
+   if (!title || !desc) {
+      document.getElementById("patientNameInput").value = "";
+      document.getElementById("eventDescInput").value = "";
+      var labels = addForm.getElementsByTagName("label");
+      for (var i = 0; i < labels.length; i++) {
+         labels[i].className = "";
+      }
+      return;
+   }
+
+   addEvent(title, desc);
+   showEvents();
+
+   if (!selectedDayBlock.querySelector(".day-mark")) {
+      selectedDayBlock.appendChild(document.createElement("div")).className = "day-mark";
+      $(".lighten-3").on("click", function(event){
+         event.preventDefault();
+      });
+   }
+
+   var inputs = addForm.getElementsByTagName("input");
+   for (var i = 0; i < inputs.length; i++) {
+      inputs[i].value = "";
+   }
+   var labels = addForm.getElementsByTagName("label");
+   for (var i = 0; i < labels.length; i++) {
+      labels[i].className = "";
+   }
+}
+
+$(document).on("click", ".list-group > a", function(){
+   var currentFireKey = $(this).attr("data-fireChildKey");
+   localStorage.setItem("currentFireKey", currentFireKey);
+
+   var currentPatientName = $(this).attr("data-patientName");
+   localStorage.setItem("currentPatientName", currentPatientName);
+
+   var currentApptDate = $(this).attr("data-apptDate");
+   localStorage.setItem("currentApptDate", currentApptDate);
+
+});
